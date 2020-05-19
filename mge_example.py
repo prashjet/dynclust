41b9b96c7671382a5100ce8306ca61569b5d8069
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2020-05-19 11:13:34 +0200

    edit comments in MGE_example

--- mge_example.py
+++ mge_example.py
@@ -1,5 +1,5 @@
 import numpy as np
-from mge import mge_fit_1d
+from mge import mge_fit_1d      # i.e. capellari's MGE code
 import matplotlib.pyplot as plt
 from scipy import stats
 
@@ -21,14 +21,15 @@ y = np.concatenate((y_g1.rvs(n1), y_g2.rvs(n2)))
 plt.scatter(x, y)
 plt.show()
 
-# find observed q value anf convert to elliptical r
+# find observed q value and convert to elliptical r
 q_obs = np.std(y)/np.std(x)
 r = np.sqrt(x**2 + (y/q_obs)**2)
 
 # fit 1D profile with capellari's code
 # ... (i) make logarithmically binned density profile
 logr = np.log10(r)
-h, loge, _ = plt.hist(logr, bins=8)    # pick N bins so that none have 0 counts
+Nbins = 8
+h, loge, _ = plt.hist(logr, bins=Nbins) # must pick Nbins so no bin has 0 counts
 e = 10.**loge
 area = np.pi * e**2.
 area_ring = area[1:] - area[0:-1]
@@ -37,23 +38,11 @@ c = np.sqrt(e[0:-1] * e[1:])
 p = mge_fit_1d.mge_fit_1d(c, rho, ngauss=10)
 p.plot()
 
-# fit with my codes
-r = r*u.kpc     # my code assumes values have astropy unit attached
-
-# fit with my MGEDiscrete1D code
+# fit with my code
+r = r*u.kpc     # assumes all values have astropy unit attached
 myMGE1D = dc.mges.MGEDiscrete1D(r, ngauss=2)
 myMGE1D.fit_mge()
 myMGE1D.plot()
-
-f, sig, q = myMGE1D.f, myMGE1D.sig, q_obs
-tracer_mge = dc.mges.TracerMGE(I = f / (2*np.pi*sig**2.) * u.pc**-2,
-                               sigma = sig * u.arcsec,
-                               q = np.array([q_obs]))
-
-
-
-
-
 
 
 
